964b953f444e18e2e265abe3f53c6ea74a72235c
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2021-02-07 18:19:24 +0530

    Completed Fetch Data

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-// const fetchBtn = document.getElementById('fetchData');
+const resultBtn = document.getElementById('submitBtn');
+const inputVal = document.getElementById('country');
 
 window.onload = (response) => {
-    fetch('https://api.covid19api.com/summary')
+    fetch('https://disease.sh/v3/covid-19/all?yesterday=false&twoDaysAgo=true&allowNull=0')
     .then(response => response.json())
     .then(data => {
         let output = '<h2>Covid Stats</h2>';
         output += '<ul>';
         output += `
-            <li>Total Confirmed: ${data.Global.TotalConfirmed}</li>
-            <li>Total Recovered: ${data.Global.TotalRecovered}</li>
+            <li>Total Confirmed: ${data.cases}</li>
+            <li>Total Recovered: ${data.recovered}</li>
         `;
         output += '</ul>';
         document.getElementById('response').innerHTML = output;
@@ -18,3 +19,30 @@ window.onload = (response) => {
         );
     // console.log('This Button works!');
 };
+
+resultBtn.addEventListener('click', (e) => {
+    fetch(`https://disease.sh/v3/covid-19/countries/${inputVal.value}?yesterday=true&twoDaysAgo=false&strict=false&allowNull=0`)
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data.deaths)
+        // console.log(data.cases)
+        console.log(data)
+        
+        let output = '<h2>Covid Stats</h2>';
+        output += '<ul>';
+        output += `
+            <li>Country: ${data.country}</li>
+            <li>Total Cases: ${data.cases}</li>
+            <li>Total Active Cases: ${data.active}</li>
+            <li>Total Recovered Cases: ${data.recovered}</li>
+            <li>Total Deaths: ${data.deaths}</li>
+        `;
+        output += '</ul>';
+        document.getElementById('response-by-country').innerHTML = output;
+    })
+    .catch(err => {
+        console.error(err);
+    });
+    e.preventDefault();
+    inputVal.value = '';
+})
